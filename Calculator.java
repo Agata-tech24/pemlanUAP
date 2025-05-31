5f4993b1c62c17f6ee64adca571f090e702268be
Cheren Agata <cherenads@student.ub.ac.id>
2025-05-31 19:53:41 +0700

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+package uap.interfaces;
+
+public interface Calculator {
+    double getVolume();
+    double getSurfaceArea();
+    double getMass();
+    double toKilogram();
+    int getShippingCost(); 
+}
